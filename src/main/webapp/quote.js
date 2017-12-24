e9853423c7b380b828ddb818be3069cb28fb70a8
Phillip Kruger <phillip.kruger@gmail.com>
2017-12-24 20:08:30 +0200

    Added Semantic UI web page

---
+++ src/main/webapp/quote.js
@@ -0,0 +1,50 @@
+/* 
+ * Semantic-UI
+ * Javascript file for index.html
+ * @author Phillip Kruger (phillip.kruger@phillip-kruger.com)
+ */
+var contextRoot = getContextRoot();
+var messages = document.getElementById("messages");
+
+
+$('document').ready(function () {
+    var url = contextRoot + "/api";
+    var quoteJson = httpGet(url);
+    var quote = JSON.parse(quoteJson);
+    writeResponse(quote.text,quote.author);
+});
+
+function writeResponse(text,author){
+    
+    messages.innerHTML += "<div class='ui statistic'>"
+        + "<div class='value'>"
+        + text
+        + "</div>"
+        + "<div class='label'>"
+        + author
+        + "</div>"
+        + "</div>";
+    
+}
+
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+
+function getContextRoot() {
+    var base = document.getElementsByTagName('base')[0];
+    if (base && base.href && (base.href.length > 0)) {
+        base = base.href;
+    } else {
+        base = document.URL;
+    }
+
+    var u = base.substr(0, base.indexOf("/", base.indexOf("/", base.indexOf("//") + 2) + 1));
+    var u = u.substr(u.indexOf("//") + 2);
+    var contextRoot = u.substr(u.indexOf("/"));
+    return contextRoot;
+}
